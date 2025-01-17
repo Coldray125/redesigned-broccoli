5659798beb484bd75cac543522e20b945100ff95
evlahov.artem <evlahov.artem@wildberries.work>
2025-01-17 13:47:17 +0300

    change text in feature

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Broccoli");
-        System.out.println("Hello Main Broccoli");
+        System.out.println("Hello Feature Broccoli");
     }
 }
