dd031a019888c327d359205d76408a4b98c49a0d
evlahov.artem <evlahov.artem@wildberries.work>
2025-09-20 16:52:54 +0300

    featureB first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is feature branch");
         System.out.println("Hello Feature Broccoli");
+
+        System.out.println("This is featureB branch");
     }
 }
