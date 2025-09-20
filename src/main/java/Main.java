db824e8a39f5ed97b56e5c4b6e0310475f6bcf16
evlahov.artem <evlahov.artem@wildberries.work>
2025-09-20 17:36:27 +0300

    reset

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is feature branch");
         System.out.println("Hello Feature Broccoli");
-
-        System.out.println("This is featureB branch");
-
-        System.out.println("This is featureA branch change");
-
-        System.out.println("This is featureB branch");
-
-        System.out.println("This is featureB branch");
     }
 }
--- src/test/BetaTest.java
+++
@@ -1,4 +0,0 @@
-public class BetaTest {
-    void betaMethod(){
-    }
-}
