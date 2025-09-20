bfe9c88d6fb58552b40427fe12b0bff42aff04b7
evlahov.artem <evlahov.artem@wildberries.work>
2025-09-20 18:04:48 +0300

    Alpha commit 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is feature branch");
         System.out.println("Hello Feature Broccoli");
-        System.out.println("This is featureA branch");
+        System.out.println("This is featureC branch");
     }
 }
